6fb6c9d3ff602a7cfad16f5745b4e7eaa73fddce
Abraham Rubio <a.rubio1224@gmail.com>
2023-09-05 13:34:08 -0400

    added server sent events

--- main.py
+++ main.py
@@ -1,12 +1,13 @@
 from datetime import datetime, timedelta
-from flask import Flask, jsonify
+import queue
+from flask import Flask, Response, jsonify
 from flask_apscheduler import APScheduler
 from flask_cors import CORS
 import requests
 import json
 import os
 
-TEST_MODE = False
+TEST_MODE = True
 
 app = Flask(__name__)
 CORS(app)
@@ -22,14 +23,25 @@ removed_internships = {}
 url = "https://raw.githubusercontent.com/SimplifyJobs/Summer2024-Internships/dev/.github/scripts/listings.json"
 local_file_path = "local_listings.json"
 mock_file_path = "mock_listings.json"  # Mock JSON file for testing
+clients = []
 
 # Flask routes
 
 
 @app.route('/new_internships', methods=['GET'])
 def get_new_internships():
-    print(read_local_json("new_internships_last_24_hours.json"))
-    return jsonify(read_local_json("new_internships_last_24_hours.json"))
+    def generate():
+        global clients
+        q = queue.Queue()
+        clients.append(q)
+        try:
+            while True:
+                result = q.get()  # This will block until a new item is available
+                yield f"data: {json.dumps(result)}\n\n"
+        except GeneratorExit:  # Happens when the client disconnects
+            clients.remove(q)
+
+    return Response(generate(), content_type='text/event-stream')
 
 # Flask route to get removed internships from the last 24 hours
 
@@ -42,6 +54,7 @@ def get_removed_internships():
 
 @app.route('/all_internships', methods=['GET'])
 def get_all_internships():
+    print(len(fetch_json(url)))
     return jsonify(fetch_json(url))
 
 
@@ -90,7 +103,7 @@ def convert_dict_to_tuple(d):
     return tuple((k, tuple(v) if isinstance(v, list) else v) for k, v in sorted(d.items()))
 
 
-@scheduler.task('cron', id='check_github_changes', hour='*/1')
+@scheduler.task('cron', id='check_github_changes', second='*/5')
 # Function to check for changes in GitHub data
 def check_github_changes():
     print("Checking for changes...\n")
@@ -133,6 +146,10 @@ def check_github_changes():
             if len(new_internships) > 0:
                 for internship in new_internships:
                     internship['timestamp'] = current_time.isoformat()
+
+                    # Notify all SSE clients about the new internships
+                for client in clients:
+                    client.put(new_internships)
 
                 # Filter and write internships from the last 24 hours
                 last_24_hours = current_time - timedelta(days=1)
